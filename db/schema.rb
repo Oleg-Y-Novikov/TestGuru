90783b1305acf92dd6daac0d958342e8925bec3b
Oleg Novikov <novikov.oleg.gsx@mail.ru>
2022-06-28 19:09:49 +0300

    feat(model): add model answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_28_144725) do
+ActiveRecord::Schema.define(version: 2022_06_28_152947) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body", null: false
+    t.boolean "correct", default: false, null: false
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
@@ -35,6 +44,7 @@ ActiveRecord::Schema.define(version: 2022_06_28_144725) do
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
 end
