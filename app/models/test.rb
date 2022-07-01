3f2f7332e8d21ebed889afd91bbd05208ea023be
Oleg Novikov <novikov.oleg.gsx@mail.ru>
2022-07-01 18:05:14 +0300

    feat(model): add method test_title_desc

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  # возвращает отсортированный по убыванию массив названий всех Тестов
+  # у которых Категория называется определённым образом
+  def self.test_title_desc(category_title)
+    join_categories = 'JOIN categories ON tests.category_id = categories.id'
+    Test.joins(join_categories).where('categories.title LIKE ?', "#{category_title}%").order(title: :desc).pluck(:title)
+  end
 end
