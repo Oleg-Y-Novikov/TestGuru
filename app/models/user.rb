68a90b404dc9ec723234c1f221d78175fdbffa08
Oleg Novikov <novikov.oleg.gsx@mail.ru>
2022-07-01 18:07:26 +0300

    feat(model user): add instance method list_all_tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # возвращает список всех Тестов, которые проходит или когда-либо проходил Пользователь
+  def list_all_tests(level)
+    join_passed_tests = 'JOIN passed_tests ON tests.id = passed_tests.test_id'
+    Test.joins(join_passed_tests).where('tests.level = ? AND passed_tests.user_id = ?', level, id)
+  end
 end
