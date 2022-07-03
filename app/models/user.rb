ef4effbac4ba1c8d821d14c3b7181780732949c9
Oleg Novikov <novikov.oleg.gsx@mail.ru>
2022-07-03 15:34:56 +0300

    fix(seed): seed refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   # возвращает список всех Тестов, которые проходит или когда-либо проходил Пользователь
   def list_all_tests(level)
     join_users_tests = 'JOIN users_tests ON tests.id = users_tests.test_id'
-    Test.joins(join_users_tests).where('tests_users.user_id = :id AND level = :level', id: id, level: level)
+    Test.joins(join_users_tests).where('users_tests.user_id = :id AND level = :level', id: id, level: level)
   end
 end
