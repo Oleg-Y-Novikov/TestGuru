7a1b29ea5595e3a4532fc1e95c6b583f3ec1f8b0
Oleg Novikov <novikov.oleg.gsx@mail.ru>
2022-07-03 12:34:35 +0300

    fix(model Test): class method refactoring: test_title_desc

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,8 @@ class Test < ApplicationRecord
   # у которых Категория называется определённым образом
   def self.test_title_desc(category_title)
     join_categories = 'JOIN categories ON tests.category_id = categories.id'
-    Test.joins(join_categories).where('categories.title LIKE ?', "#{category_title}%").order(title: :desc).pluck(:title)
+    joins(join_categories).where(
+      'categories.title LIKE :title', title: "#{category_title}%"
+    ).order(title: :desc).pluck(:title)
   end
 end
