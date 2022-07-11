e6288dd9a9ced18613d323e20f86bfadaf14d0a0
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2022-07-11 16:09:50 +0300

    refactor(model Test): replaced class method (test_title_desc) with scope method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  # возвращает объект ActiveRecord::Relation с отфильтрованными тестами по уровню сложности
   scope :easy_level,   -> { where(level: 0..1) }
   scope :medium_level, -> { where(level: 2..4) }
   scope :hard_level,   -> { where(level: 4..) }
+
+  # возвращает отсортированный по убыванию массив названий всех Тестов
+  # у которых Категория называется определённым образом
+  scope :test_title_desc, lambda { |category_title|
+                            joins(:category)
+                              .where(categories: { title: category_title })
+                              .order(title: :desc)
+                              .pluck(:title)
+                          }
 
   belongs_to :author, class_name: 'User'
   belongs_to :category
@@ -11,13 +21,4 @@ class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :tests_user, dependent: :destroy
   has_many :users, through: :tests_user, dependent: :destroy
-
-  # возвращает отсортированный по убыванию массив названий всех Тестов
-  # у которых Категория называется определённым образом
-  def self.test_title_desc(category_title)
-    joins(:category)
-      .where(categories: { title: category_title })
-      .order(title: :desc)
-      .pluck(:title)
-  end
 end
