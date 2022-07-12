2669d8f82a693c8911077313f1e9780e997de390
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2022-07-12 14:00:21 +0300

    fix: fixed the logic of checking the number of answers to one question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 class Answer < ApplicationRecord
-  scope :correct_answers, -> { where(correct: true) }
-
   belongs_to :question
 
   validates :body, presence: true
+  validate :validate_quantity_answers, on: :create
+
+  scope :correct_answers, -> { where(correct: true) }
+
+  private
+
+  # проверяет, что у одного Вопроса может быть не более 4-х Ответов
+  def validate_quantity_answers
+    errors.add(:quantity_answers, I18n.t('model.answer.limit_answers')) if question.answers.count >= 4
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -6,12 +6,4 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :body, presence: true
-  validate :validate_quantity_answers
-
-  private
-
-  # проверяет, что у одного Вопроса может быть не менее 1-го и не более 4-х ответов
-  def validate_quantity_answers
-    errors.add(:quantity_answers, 'question should have from 1 to 4 answers') unless (1..4).include? answers.size
-  end
 end
