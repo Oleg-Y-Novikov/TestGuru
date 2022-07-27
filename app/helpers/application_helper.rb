1f425421d56ddd23bc985a6fc96e84966621df1e
Oleg Novikov <novikov.oleg.gsx@mail.ru>
2022-07-27 13:10:25 +0300

    feat(helpers): added new methods to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  # Возвращает полный заголовок на основе заголовка страницы.
+  def full_title(page_title = '')
+    base_title = 'TestGuru'
+    page_title.blank? ? base_title : "#{page_title} | #{base_title}"
+  end
+
+  # Возвращает текущий год.
+  def current_year
+    Date.today.year
+  end
+
+  # Возвращает url на GitHub репозиторий
+  def github_url(author, repo)
+    %(https://github.com/#{author}/#{repo}/)
+  end
 end
