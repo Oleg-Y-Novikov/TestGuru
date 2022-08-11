116d3d3306a0b4ccd7dd9a78531472766e1315d4
Oleg Novikov <novikov.oleg.gsx@mail.ru>
2022-08-11 10:46:38 +0300

    feat(tests_user): implemented a tests_user resource

--- app/models/tests_user.rb
+++ app/models/tests_user.rb
@@ -3,4 +3,69 @@
 class TestsUser < ApplicationRecord
   belongs_to :test
   belongs_to :user
+  belongs_to :current_question, class_name: 'Question'
+
+  before_validation :before_validation_set_first_question, on: :create
+  before_update :before_update_set_next_question
+
+  # увеличивает счетчик верных ответов и обновляет объект TestsUser
+  def accept!(answer_ids)
+    self.correct_questions += 1 if correct_answer?(answer_ids)
+
+    save!
+  end
+
+  # возвращает true если тест пройден до конца в противном случае false
+  def completed?
+    completed
+  end
+
+  # обновляет поля у объекта TestsUser на стартовые значения для прохождения теста
+  # метод формирует прямой запрос в базу минуя валидации и колбэки
+  def start_test_user_params!
+    update_columns(completed: false, correct_questions: 0)
+  end
+
+  def test_passage_success?
+    result >= 85
+  end
+
+  def result
+    (correct_questions * 100.0 / test.questions.count).to_i
+  end
+
+  def current_question_number
+    test.questions.order(:id).where('id < ?', current_question.id).size + 1
+  end
+
+  def total_amount_questions
+    test.questions.count
+  end
+
+  private
+
+  # проверяет корректность данных пользователем ответов
+  # возвращает true если все ответы верные, в противном случае false
+  def correct_answer?(answer_ids)
+    return false if answer_ids.blank?
+
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+  end
+
+  # возвращает объект ActiveRecord корректных ответов на текущий вопрос
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def before_update_set_next_question
+    self.current_question = test.questions.where('id > ?', current_question).order(:id).first
+    return if current_question.present?
+
+    self.completed = true
+    self.current_question = test.questions.first
+  end
+
+  def before_validation_set_first_question
+    self.current_question = test.questions.first
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_11_140830) do
+ActiveRecord::Schema.define(version: 2022_08_05_090239) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -52,6 +52,10 @@ ActiveRecord::Schema.define(version: 2022_07_11_140830) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "current_question_id"
+    t.integer "correct_questions", default: 0
+    t.boolean "completed", default: false
+    t.index ["current_question_id"], name: "index_tests_users_on_current_question_id"
     t.index ["test_id", "user_id"], name: "index_tests_users_on_test_id_and_user_id", unique: true
     t.index ["test_id"], name: "index_tests_users_on_test_id"
     t.index ["user_id"], name: "index_tests_users_on_user_id"
@@ -70,6 +74,7 @@ ActiveRecord::Schema.define(version: 2022_07_11_140830) do
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
+  add_foreign_key "tests_users", "questions", column: "current_question_id"
   add_foreign_key "tests_users", "tests"
   add_foreign_key "tests_users", "users"
 end
