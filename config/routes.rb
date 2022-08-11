13140f18762f88b66faa272ea7e554cdb12feb80
Oleg Novikov <novikov.oleg.gsx@mail.ru>
2022-08-11 10:36:38 +0300

    feat(routes): add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'tests#index'
+
+  get '/about',  to: 'static_pages#about'
+  get '/author', to: 'static_pages#author'
+
   resources :tests do
-    resources :questions, shallow: true, except: :index
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true, except: :index
+    end
+
+    member do
+      post :start
+    end
+  end
+
+  resources :tests_users, only: %i[show update] do
+    member do
+      get :result
+      put :restart
+    end
   end
 end
