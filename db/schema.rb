b72161b4d93cfbe6ae30b1d3d489122974e15a56
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2022-10-16 16:32:20 +0300

    feat: added new expressions and seeds refactoring

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_05_090239) do
+ActiveRecord::Schema.define(version: 2022_10_14_183318) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2022_08_05_090239) do
     t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
