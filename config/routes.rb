a87f103d29d8a8aca24f8bd556cb07a5fbf8d06c
Oleg-Y-Novikov <novikov.oleg.gsx@mail.ru>
2022-10-22 12:14:09 +0300

    feat: customization devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@
 Rails.application.routes.draw do
   root 'tests#index'
 
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
-
-  #resources :users, except: :index
+  devise_for :users, path: :gurus, controllers: { sessions: 'users/sessions' },
+                     path_names: { sign_in: :login, sign_out: :logout }
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
